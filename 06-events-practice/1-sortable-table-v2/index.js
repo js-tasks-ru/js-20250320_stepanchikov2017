b5ce3c6b9bbe0be09f4b9f2d98cf5c2b643946c9
SupStep <istepanchikov@scid.ru>
2025-04-11 15:15:43 +0300

    правки 6.1

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,79 +1,50 @@
-import SortableTable from '../../05-dom-document-loading/2-sortable-table-v1/index.js'
+import SortableTable from '../../05-dom-document-loading/2-sortable-table-v1/index.js';
 
 export default class SortableTablev2 extends SortableTable {
-  ArrowElement;
-  
   constructor(headersConfig, {
     data = [],
     sorted = {}
   } = {}) {
-    super(headersConfig, data)
-    this.data = data;
+    super(headersConfig, data);
     this.sorted = sorted;
 
-    this.ArrowElement = this.createArrowElement();
     this.createListeners();
     this.sortTable();
   }
 
-  createArrowTemplate() {
-    return `
-      <span data-element="arrow" class="sortable-table__sort-arrow">
-        <span class="sort-arrow"></span>
-      </span>
-    `;
+  createListeners() {
+    this.subElements.header.addEventListener('click', this.handlerHeaderClick);
   }
 
-  createArrowElement() {
-    const ArrowElement = document.createElement('div');
-    ArrowElement.innerHTML = this.createArrowTemplate();
-    return ArrowElement.firstElementChild;
+  removeListeners() {
+    this.subElements.header.removeEventListener('click', this.handlerHeaderClick);
+  }
+
+  sortTable() {
+    if (this.sorted?.id && this.sorted?.order) {
+      this.clearCellOrder();
+      this.sort(this.sorted.id, this.sorted.order);
+    }
   }
 
   clearCellOrder() {
     const cells = this.element.querySelectorAll('.sortable-table__header .sortable-table__cell');
-    cells.forEach(cell => {
+    for (const cell of cells) {
       cell.setAttribute('data-order', '');
-    });
-  }
-
-  updateCell(field, order) {
-    const sortableCell = this.element.querySelector(`[data-id="${field}"]`);
-    sortableCell.setAttribute('data-order', order);
-    sortableCell.append(this.ArrowElement);
-  }
-
-  sortTable() {
-    this.sort(this.sorted.id, this.sorted.order);
-    this.updateCell(this.sorted.id, this.sorted.order);
-  }
-
-  handlerHeaderClick(e) {
-    const currentCell = e.target.closest('.sortable-table__cell');
-
-    if (!currentCell || currentCell.dataset.sortable === 'false') return;
-
-    const field = currentCell.dataset.id;
-    const currentOrder = currentCell.dataset.order;
-
-    let newOrder;
-    if (currentOrder === '' || currentOrder === undefined) {
-      newOrder = 'desc';
-    } else {
-      newOrder = currentOrder === 'asc' ? 'desc' : 'asc';
     }
+  }
 
+  handlerHeaderClick = event => {
+    const cell = event.target.closest('.sortable-table__cell');
+    if (!cell || cell.dataset.sortable === 'false') return;
+
+    const field = cell.dataset.id;
+    const currentOrder = cell.dataset.order;
+    const newOrder = currentOrder === 'asc' ? 'desc' : 'asc';
+
+    this.clearCellOrder();
     this.sort(field, newOrder);
-    this.updateCell(field, newOrder);
-  }
-
-  createListeners() {
-    this.subElements.header.addEventListener('click', (event) => this.handlerHeaderClick(event));
-  }
-
-  removeListeners() {
-    this.subElements.header.removeEventListener('click', (event) => this.handlerHeaderClick(event));
-  }
+  };
 
   remove() {
     this.element.remove();
